3de7615fdc4c2c3e1894d159e8d5317d61c37d10
Jaceimet <84209692+Jaceimet@users.noreply.github.com>
2023-02-06 11:51:36 -0800

    Generated new data and making new main method for after returning demo

--- spring-demo-aop-z-after-returning/src/com/luv2code/aopdemo/Account.java
+++ spring-demo-aop-z-after-returning/src/com/luv2code/aopdemo/Account.java
@@ -28,6 +28,14 @@ public class Account {
 	public void setLevel(String level) {
 		this.level = level;
 	}
+
+
+	@Override
+	public String toString() {
+		return "Account [name=" + name + ", level=" + level + "]";
+	}
+	
+	
 	
 	
 	
