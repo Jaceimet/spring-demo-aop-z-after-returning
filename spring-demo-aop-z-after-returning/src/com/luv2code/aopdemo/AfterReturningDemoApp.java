e516c56b96f70344ee5c28ac0bbfeb727b5fb787
Jaceimet <84209692+Jaceimet@users.noreply.github.com>
2023-02-06 11:57:38 -0800

    Added call to new findAccounts method in after return main method
    printout successful

--- spring-demo-aop-z-after-returning/src/com/luv2code/aopdemo/AfterReturningDemoApp.java
+++ spring-demo-aop-z-after-returning/src/com/luv2code/aopdemo/AfterReturningDemoApp.java
@@ -1,5 +1,7 @@
 package com.luv2code.aopdemo;
 
+import java.util.List;
+
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import com.luv2code.aopdemo.dao.AccountDAO;
@@ -18,7 +20,16 @@ public class AfterReturningDemoApp {
 		AccountDAO theAccountDAO = context.getBean("accountDAO", AccountDAO.class);
 		
 		
-		//
+		// call the method to find the accounts
+		List<Account> theAccounts = theAccountDAO.findAccounts();
+		
+		// display the accounts
+		System.out.println("\n\nMain Program: AfterReturningDemoApp");
+		System.out.println("---");
+		
+		System.out.println(theAccounts);
+		
+		System.out.println("\n");
 		
 		// close the context
 		context.close();
